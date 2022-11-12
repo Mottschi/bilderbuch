036fb3121659b7b389077ec5e7425aba060e4d0e
Sebastian Mottschall <srophkor@gmail.com>
2022-11-12 22:06:46 +0100

    empty view functions created for all tasks, login implementation begun

--- administrator/urls.py
+++ administrator/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path("", views.view_display_betreiberliste, name="betreiberliste"),
+    path("login", views.view_login, name="login"),
+    path("logout", views.view_logout, name="logout"),
+    path('betreiber/create', views.view_create_betreiber, name='create_betreiber'),
+    path('betreiber/edit', views.view_create_betreiber, name='create_betreiber'),
+    path('betreiber/delete', views.view_create_betreiber, name='create_betreiber'),
 ]
--- administrator/views.py
+++ administrator/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required, user_passes_test
+
+from .forms import LoginForm
+
+def is_sysadmin(user):
+    return user.groups.filter(name='systemadmin').exists()
 
 # Create your views here.
+def view_login(request):
+    '''
+    /PF0010/ Ein nicht eingeloggter Administrator kann sich unter Angabe des 
+    Benutzernamens und Passworts am System anmelden.
+    '''
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse('administrator:betreiberliste'))
+            else:
+                print('unable to login')
+                return render(request, 'endnutzer/login.html', {
+                    'error': 'Fehler beim Einloggen',
+                })
+
+    return render(request, 'administrator/login.html', {
+        'form': LoginForm(),
+    })
+
+
+@login_required
+def view_logout(request):
+    '''
+    /PF0020/ Ein eingeloggter Administrator kann sich vom System abmelden.
+    '''
+
+@login_required
+@user_passes_test(is_sysadmin)
+def view_display_betreiberliste(request):
+    '''
+    /PF0040/ Ein eingeloggter Administrator kann eine Liste der bestehenden 
+    Betreiberkonten einsehen.
+    '''
+    render(request, 'administrator/index.html')
+
+@login_required
+@user_passes_test(is_sysadmin)
+def view_create_betreiber(request):
+    '''
+    /PF0030/ Ein eingeloggter Administrator kann neue Betreiberkonten für 
+    Verlagsmitarbeiter einrichten
+    '''
+
+@login_required
+@user_passes_test(is_sysadmin)
+def view_edit_betreiber(request):
+    '''
+    /PF0050/ Ein eingeloggter Administrator kann die Daten von Betreiberkonten editieren.
+    '''
+
+@login_required
+@user_passes_test(is_sysadmin)
+def view_delete_betreiber(request):
+    '''
+    /PF0060/ Ein eingeloggter Administrator kann bestehende Betreiberkonten löschen.
+    '''
