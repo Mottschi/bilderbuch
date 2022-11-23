3e98ade422b85609ddfb718a7a3ef99d19879bfe
christoph <christoph.schrader@gmx.de>
2022-11-23 15:10:40 +0100

    loginFormCss

--- administrator/forms.py
+++ administrator/forms.py
@@ -4,6 +4,9 @@ from betreiber.models import User
 class LoginForm(forms.Form):
 	username = forms.CharField(max_length=150)
 	password = forms.CharField(widget=forms.PasswordInput)
+	
+	username.widget.attrs.update({'class': 'form-control'})
+	password.widget.attrs.update({'class': 'form-control'})
 
 
 class BetreiberForm(forms.ModelForm):
