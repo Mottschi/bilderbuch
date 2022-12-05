2bc3ba68a394d9d671e52f4ce51afa73dea3d5b5
Sebastian Mottschall <srophkor@gmail.com>
2022-12-05 23:51:24 +0100

    Email setup for production

--- projektbilderbuch/settings.py
+++ projektbilderbuch/settings.py
@@ -152,7 +152,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'betreiber.User'
 
 # Setting up email backend for password emails
-EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-EMAIL_FILE_PATH = 'tmp/app-messages'
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+    EMAIL_FILE_PATH = 'tmp/app-messages'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = 'projekt.bilderbuch@gmail.com'
+    EMAIL_HOST_PASSWORD = os.getenv('GMAIL_PW')
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_USE_SSL = False
+    DEFAULT_FROM_EMAIL = 'projekt.bilderbuch@gmail.com'
+
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = None 
