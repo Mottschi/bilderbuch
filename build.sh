4ae7e7d62cd0c043752c6fc382e81fa79664056f
Sebastian Mottschall <srophkor@gmail.com>
2022-12-05 19:20:52 +0100

    Adding pip upgrade/force reinstall to build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 # exit on error
 set -o errexit
 
-pip install --upgrade pippip install --force-reinstall -U setuptools
-
 pip install -r requirements.txt
+
+pip install --upgrade pippip install --force-reinstall -U setuptools
 
 python manage.py collectstatic --no-input
 python manage.py migrate
