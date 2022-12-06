996fdb6805effd54413a38e17ccff93b06879e81
Sebastian Mottschall <srophkor@gmail.com>
2022-12-06 13:51:28 +0100

    Trying without debug

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
-pip install --upgrade pip
-pip install --force-reinstall -U setuptools
+# pip install --upgrade pip
+# pip install --force-reinstall -U setuptools
 
 python manage.py collectstatic --no-input
 python manage.py migrate
