874cb06ce5bb559a5e8bb3ffd73ec5177cd256d8
Sebastian Mottschall <srophkor@gmail.com>
2022-12-06 13:54:56 +0100

    back to debug

--- projektbilderbuch/settings.py
+++ projektbilderbuch/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 RENDER = 'RENDER' in os.environ
 
 # If we are not on RENDER, we are in debug environment
-DEBUG = False
+DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 if RENDER:
